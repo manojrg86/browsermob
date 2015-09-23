e6ed391d209068c88edb3722186397a341024b71
manojrg86 <manojrg86@gmail.com>
2015-09-23 13:48:36 -0500

    adding required jars

--- browsermobsample/src/main/java/com/browsermobsample/SeleniumBrowserMob.java
+++ browsermobsample/src/main/java/com/browsermobsample/SeleniumBrowserMob.java
@@ -1,10 +1,14 @@
 package com.browsermobsample;
 
 import java.net.URL;
+import java.util.List;
 
 import net.lightbody.bmp.BrowserMobProxyServer;
 import net.lightbody.bmp.client.ClientUtil;
 import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.core.har.HarEntry;
+import net.lightbody.bmp.core.har.HarLog;
+import net.lightbody.bmp.core.har.HarPage;
 
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
@@ -14,12 +18,15 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SeleniumBrowserMob {
 	public static void main(String args[]){
+		WebDriver driver = null;
 		try{
-			BrowserMobProxyServer proxy = new BrowserMobProxyServer();
-			proxy.start(0);
+			System.out.println("In");
+			BrowserMobProxyServer proxyServer = new BrowserMobProxyServer();
+			proxyServer.start(0);
+			System.out.println("start proxy");
 
 			// get the Selenium proxy object
-			Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+			Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxyServer);
 
 			// configure it as a desired capability
 			DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -28,20 +35,62 @@ public class SeleniumBrowserMob {
 
 			// start the browser up
 			URL url =new URL("http://atubh3072p.prod.ch3.s.com:4444/wd/hub");
-			WebDriver driver = new RemoteWebDriver(url,capabilities);
+			driver = new RemoteWebDriver(url,capabilities);
+			System.out.println("start driver");
 
 			// create a new HAR with the label "yahoo.com"
-			proxy.newHar("sears.com");
+			proxyServer.newHar("sears.com");
 
 			// open yahoo.com
 			driver.get("http://www.sears.com");
 
 			// get the HAR data
-			Har har = proxy.getHar();
-			driver.quit();
-			proxy.stop();
+			Har har = proxyServer.getHar();
+			readHarFile(har);
+			/*File harFile = new File("traffic.har");
+			har.writeTo(harFile);*/
+			proxyServer.stop();
 		}catch(Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}finally{
+			if(null!=driver)
+				driver.quit();
 
 		}
+	}
+	public static void readHarFile(Har har){
+
+		try
+		{
+			// Access all elements as objects
+			HarLog log=har.getLog();
+			//HarBrowser browser =log.getBrowser();
+
+			// Used for loops
+			List<HarPage> pages = log.getPages();
+			List<HarEntry> hentry = log.getEntries(); 
+
+			for (HarPage page : pages)
+			{
+				System.out.println("page start time: "+ page.getStartedDateTime());
+				System.out.println("page id: " + page.getId());
+				System.out.println("page title: " + page.getTitle());
+			}
+
+			//Output "response" code of entries.
+			for (HarEntry entry : hentry)
+			{
+				System.out.println("request code: " + entry.getRequest().getMethod()); //Output request type
+				System.out.println("response code: " + entry.getRequest().getUrl()); //Output url of request
+				System.out.println("response code: " + entry.getResponse().getStatus()); // Output the 
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			//fail("IO exception during test");
+		}
+
 	}
 }
